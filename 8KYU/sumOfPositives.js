b9e3bf8f77e8920a806f5267cf335b3f704589db
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-03-20 07:47:10 -0400

    CodeWars 2023-3-20

---
+++ 8KYU/sumOfPositives.js
@@ -0,0 +1,22 @@
+// CodeWars 2023-3-20 //
+
+/* You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+
+// SOLUTION //
+
+function positiveSum(arr){
+    let sum = 0
+    arr.forEach(ele => {
+        if(ele < 0){
+            sum += ele
+        }
+    })
+    return sum
+}
+
+// END //
