011c217db9977b19efd6b30b8eee07f8a0f363cb
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-03-26 10:53:12 -0400

    Update needleInHaystack.js

--- 8KYU/needleInHaystack.js
+++ 8KYU/needleInHaystack.js
@@ -1,6 +1,6 @@
-haystack.js
+// haystack.js 
 
-// 
+/* 
 Can you find the needle in the haystack?
 
 Write a function `findNeedle()` that takes an `array` full of junk but containing one `"needle"`
@@ -13,7 +13,7 @@ After your function finds the needle it should return a message (as a string) th
 ```
 ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
 ```
-//
+*/
 
 // SOLUTION //
 
