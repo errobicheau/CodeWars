9ee95cb90d4b6c823c7b2f65a9c14e8e1123008a
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-03-26 18:29:55 -0400

    Upload of 1 8KYU kata and 1 7KYU kata

---
+++ 7KYU/catYearsDogYears.js
@@ -0,0 +1,31 @@
+*/
+I have a cat and a dog.
+
+I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+Return their respective ages now as [humanYears,catYears,dogYears]
+
+NOTES:
+
+humanYears >= 1
+humanYears are whole numbers only
+Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+Dog Years
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that
+/*
+
+
+// SOLUTION //
+
+const humanYearsCatYearsDogYears = function(age) {
+if (age == 1) return [1, 15, 15]
+if (age == 2) return [2, 24, 24]
+if (age > 2) return [age, (age-2) * 4 + 24, (age-2) * 5 + 24]
+}
+
+// END //
---
+++ 8KYU/countingSheep.js
@@ -0,0 +1,29 @@
+*/
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+/*
+
+// SOLUTION //
+
+function countSheep (arrayOfSheep) {
+	let count = 0
+	arrayOfSheep.ForEach(sheep => {
+	if(sheep) {
+		count++
+	}
+	})
+	return count
+}
+
+// END //
