d6bfc70b0cb31974c4a9be1d2205e329752f980a
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-04-02 18:21:50 -0400

    Daily codewars
    -Upload of descendingOrder.js
    -Upload of contaminationString.js
    - Fixed comment formatting on countingSheep.js

--- 8KYU/countingSheep.js
+++ 8KYU/countingSheep.js
@@ -1,4 +1,4 @@
-*/
+/*
 Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 
 For example,
@@ -12,7 +12,7 @@ For example,
 The correct answer would be 17.
 
 Hint: Don't forget to check for bad values like null/undefined
-/*
+*/
 
 // SOLUTION //
 
