3d42edfeef0ba26778a85619983ae9c249839a3b
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-04-25 08:08:23 -0400

    CodeWars 2023-4-25

--- 8KYU/sumOfPositives.js
+++ 8KYU/sumOfPositives.js
@@ -9,14 +9,18 @@ Note: if there is nothing to sum, the sum is default to 0.
 
 // SOLUTION //
 
-function positiveSum(arr){
-    let sum = 0
-    arr.forEach(ele => {
-        if(ele < 0){
-            sum += ele
-        }
-    })
-    return sum
-}
+// function positiveSum(arr){
+//     let sum = 0
+//     arr.forEach(ele => {
+//         if(ele < 0){
+//             sum += ele
+//         }
+//     })
+//     return sum
+// }
+
+// REFACTORED SOLUTION //
+
+const positiveSum = arr => arr.reduce((a,b)=> a + (b > 0 ? b : 0), 0)
 
 // END //
