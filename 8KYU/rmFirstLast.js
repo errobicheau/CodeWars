49ae5072d277ed5614f4a37dd6446ff19113f0c9
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-05-01 12:49:40 -0400

    CodeWars 2023-4-28

---
+++ 8KYU/rmFirstLast.js
@@ -0,0 +1,18 @@
+// CodeWars 2023-4-28 // 
+
+/* 
+You are given a string containing a sequence of character sequences separated by commas.
+
+Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+*/
+
+
+// SOLUTION //
+
+function array(arr){
+    return arr.split(",").slice(1,-1).join(" ") || null
+  }
+
+// END //
