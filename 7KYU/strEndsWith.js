3d210eb8a03a1006c68a2b16e0e86c8d6c8d4677
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-05-02 12:29:10 -0400

    CodeWars 2023-5-2

---
+++ 7KYU/strEndsWith.js
@@ -0,0 +1,17 @@
+// CodeWars 2023-5-2 //
+
+/* 
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+*/
+
+// SOLUTION //
+
+function solution(str, ending){
+    return str.endsWith(ending);
+}
+
+// REFACTORED SOLUTION //
+
+const solution = (str, ending) => str.endsWith(ending)
+
+// END //
