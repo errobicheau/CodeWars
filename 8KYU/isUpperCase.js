f4ba74aa7d01d16429d9a867c9650a31719a8174
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-05-08 09:30:00 -0400

    CodeWars 2023-5-8

--- 8KYU/isUpperCase.js
+++ 8KYU/isUpperCase.js
@@ -1,7 +1,6 @@
 // CodeWars 2023-5-8 //
 
-/* 
-Is the string uppercase?
+/* Is the string uppercase?
 Task
 Create a method to see whether the string is ALL CAPS.
 
@@ -12,8 +11,7 @@ Examples (input -> output)
 "HELLO I AM DONALD" -> True
 "ACSKLDFJSgSKLDFJSKLDFJ" -> False
 "ACSKLDFJSGSKLDFJSKLDFJ" -> True
-In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
-*/
+In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS. */
 
 // SOLUTION //
 
