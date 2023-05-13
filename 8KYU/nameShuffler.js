c0c902a43eccade98f3ceb3320c850f245baa7e2
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-05-13 09:45:03 -0400

    CodeWars 2023-5-13

---
+++ 8KYU/nameShuffler.js
@@ -0,0 +1,10 @@
+// CodeWars 2023-5-13 //
+
+/* Description: 
+Write a function that returns a string in which firstname is swapped with last name. */
+
+// SOLUTION //
+
+const nameShuffler = str => str.split(' ').reverse().join(' ')
+
+// END //
