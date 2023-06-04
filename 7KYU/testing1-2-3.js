624d7d861b49e0527016577f4a9ce792e2e7418a
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-06-04 13:01:42 -0400

    CodeWars 2023-6-4

---
+++ 7KYU/testing1-2-3.js
@@ -0,0 +1,24 @@
+// CodeWars 2023-6-4 // 
+
+/* Description: 
+Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+Examples: (Input --> Output)
+
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3: c"]*/
+
+// SOLUTION //
+
+function number (array){
+    for(let i = 0; i<array.length; i++) {
+      array.splice(i, 1, `${i+1}: ${array[i]}`)
+      }
+    return array
+  }
+
+// END //
