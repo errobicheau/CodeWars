f7c25a94180492695ca32b17c5f9950b9321deed
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-06-12 16:02:12 -0400

    CodeWars 2023-6-13

---
+++ 8KYU/removeExclamation.js
@@ -0,0 +1,18 @@
+// CodeWars 2023-6-12 //
+
+/* Description:
+Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+Examples
+remove("Hi!") == "Hi"
+remove("Hi!!!") == "Hi!!"
+remove("!Hi") == "!Hi"
+remove("!Hi!") == "!Hi"
+remove("Hi! Hi!") == "Hi! Hi"
+remove("Hi") == "Hi" */
+
+// SOLUTION //
+
+const remove = string => string.endsWith('!') ? string.slice(0, -1) : string
+
+// END //
