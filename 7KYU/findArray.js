2be3bc117adbaeba21ba0b2cd53ed0895f7785c5
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-06-16 11:11:46 -0400

    CodeWars 2023-6-16

---
+++ 7KYU/findArray.js
@@ -0,0 +1,46 @@
+// CodeWars 2023-6-16 //
+
+/* Description: 
+You are given two arrays arr1 and arr2, where arr2 always contains integers.
+
+Write a function such that:
+
+For arr1 = ['a', 'a', 'a', 'a', 'a'], arr2 = [2, 4] the function returns ['a', 'a']
+
+For arr1 = [0, 1, 5, 2, 1, 8, 9, 1, 5], arr2 = [1, 4, 7] the function returns [1, 1, 1]
+
+For arr1 = [0, 3, 4], arr2 = [2, 6] the function returns [4]
+
+For arr1=["a","b","c","d"] , arr2=[2,2,2], the function returns ["c","c","c"]
+
+For arr1=["a","b","c","d"], arr2=[3,0,2] the function returns ["d","a","c"]
+
+Note that when an element inside arr2 is greater than the index of the last element of arr1 no element of arr1 should be added to the resulting array. If either arr1 or arr2 is empty, you should return an empty arr (empty list in python, empty vector in c++). Note for c++ use std::vector arr1, arr2. */
+
+// ORIGINAL SOLUTION //
+
+function findArray(arr1, arr2){
+ 
+    let newArr = []
+    if(arr1.length === 0 || arr2.length === 0){
+      return []
+    }else{
+    for(let i = 0; i<arr2.length; i++) { 
+      newArr.push(arr1[arr2[i]])
+      }
+    }
+    return newArr 
+  }
+
+
+// REFACTORED SOLUTION //
+
+const findArray = (arr1, arr2) => {
+  if(arr1.length === 0 || arr2.length === 0) {return []} 
+  
+  newArr = [];
+  arr2.map(arr => newArr.push(arr1[arr]))
+  return newArr
+}
+
+// END //
