8aac1ef3dae1fb52d61b622366f9ee1b7df0c804
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-06-18 15:29:54 -0400

    CodeWars 2023-06-18

---
+++ JavaScript/8KYU/templateStrings.js
@@ -0,0 +1,19 @@
+// CodeWars 2023-06-18 //
+
+/* Description: 
+Template Strings
+Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+Task
+Your task is to return the correct string using the Template String Feature.
+Input
+Two Strings, no validation is needed.
+Output
+You must output a string containing the two strings with the word ```' are '``` */
+
+// SOLUTION //
+
+var TempleStrings = function(obj, feature) {
+    return `${obj} are ${feature}`
+  }
+
+// END //
