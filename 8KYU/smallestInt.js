70599797af35238d8d87f63a3528e283c5089660
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-06-24 11:56:52 -0400

    CodeWars 2023-6-24

---
+++ 8KYU/smallestInt.js
@@ -0,0 +1,21 @@
+// CodeWars 2023-6-24 //
+
+/* Descrpition: 
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty. */
+
+// SOLUTION //
+
+class SmallestIntegerFinder {
+  
+findSmallestInt = args => {
+      return (args.sort((a, b) => a-b)[0])
+    }
+}
+
+// END //
