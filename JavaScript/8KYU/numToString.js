063010d0f37d67e4b897899ca0ebe7333cfe1f25
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-06-25 15:41:31 -0400

    CodeWars 2023-06-25

---
+++ JavaScript/8KYU/numToString.js
@@ -0,0 +1,19 @@
+// CodeWars 2023-06-23 //
+
+/* Description: 
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100" */
+
+// SOLUTION //
+
+function numberToString (num) {
+    return num.toString();
+}
+
+// END //
