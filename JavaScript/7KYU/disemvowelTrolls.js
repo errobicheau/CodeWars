813e2de3d2d6e5063172af439d4db736b355ccc5
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-07-16 10:55:10 -0400

    CodeWars 2023-07-16

--- JavaScript/7KYU/disemvowelTrolls.js
+++ JavaScript/7KYU/disemvowelTrolls.js
@@ -6,7 +6,7 @@ Your task is to write a function that takes a string and return a new string wit
 
 For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
 
-Note: for this kata y isn't considered a vowel. */
+Note: for this kata 'y' isn't considered a vowel. */
 
 // SOLUTION //
 
