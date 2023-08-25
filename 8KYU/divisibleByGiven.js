13719479962a0afaa418b654fcd3bd14b699a3b0
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-08-24 23:19:39 -0400

    CodeWars 2023-08-24

--- 8KYU/divisibleByGiven.js
+++ 8KYU/divisibleByGiven.js
@@ -1,4 +1,4 @@
-// CodeWars 2023-24-23 //
+// CodeWars 2023-08-24 //
 
 /* Description:
 Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
