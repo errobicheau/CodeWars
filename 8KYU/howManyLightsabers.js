442b56917d814938df4e2824227f07177e3278fd
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-09-11 21:08:32 -0400

    CodeWars 2023-9-11

---
+++ 8KYU/howManyLightsabers.js
@@ -0,0 +1,24 @@
+// CodeWars 2023-9-11 //
+
+/* Description: 
+Inspired by the development team at Vooza, write the function that
+
+accepts the name of a programmer, and
+returns the number of lightsabers owned by that person.
+The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+Note: your function should have a default parameter.
+
+For example(Input --> Output):
+
+"anyone else" --> 0
+"Zach" --> 18
+*/
+
+// SOLUTION //
+
+function howManyLightsabersDoYouOwn(name) {
+    return name == 'Zach' ? 18 : 0
+}
+
+// END //
