dcf0e1eaa0ba70b2941af1ff36d07474554f37d4
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-09-13 09:59:51 -0400

    CodeWars 2023-9-13

---
+++ 8KYU/getPlanetByID.js
@@ -0,0 +1,28 @@
+// CodeWars 2023-9-13 //
+
+/* Description: 
+
+The function is not returning the correct values. Can you figure out why?
+
+Example (Input --> Output ):
+
+3 --> "Earth"
+*/
+
+// SOLUTION //
+
+function getPlanetName(id){
+
+    switch(id){
+      case 1: return 'Mercury'
+      case 2: return 'Venus'
+      case 3: return 'Earth'
+      case 4: return 'Mars'
+      case 5: return 'Jupiter'
+      case 6: return 'Saturn'
+      case 7: return 'Uranus'
+      case 8: return 'Neptune'
+      }
+  }
+
+// END //
