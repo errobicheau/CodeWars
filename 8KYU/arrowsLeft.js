fe09e721618995db6e407d970fcfe17f15886b7b
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-11-12 11:04:21 -0500

    CodeWars 2023-11-12

---
+++ 8KYU/arrowsLeft.js
@@ -0,0 +1,25 @@
+// CodeWars 2023-11-12 //
+
+/* Description: 
+Check your arrows
+You have a quiver of arrows, but some have been damaged. The quiver contains arrows with an optional range information (different types of targets are positioned at different ranges), so each item is an arrow.
+You need to verify that you have some good ones left, in order to prepare for battle:
+
+anyArrows([{range: 5}, {range: 10, damaged: true}, {damaged: true}])
+If an arrow in the quiver does not have a damaged status, it means it's new.
+
+The expected result is a boolean, indicating whether you have any good arrows left. */
+
+// SOLUTION //
+
+function anyArrows(arrows){
+    return arrows.some(arrow => !arrow.damaged);
+  }
+
+// TEST CASES //
+
+console.log(anyArrows([])) // false
+console.log(anyArrows([{range: 5}, {range: 10, damaged: true}, {damaged: true}])) //true
+console.log(anyArrows([{range: 10, damaged: true}, {damaged: true}]))
+
+// END //
