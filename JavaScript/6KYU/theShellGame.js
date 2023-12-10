b763cc8d098dd06ba19f502dd76c88a3b36c4c7e
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-12-09 22:10:41 -0500

    CodeWars 2023-12-09

---
+++ JavaScript/6KYU/theShellGame.js
@@ -0,0 +1,34 @@
+// CodeWars 2023-12-09 //
+
+/* Description: "The Shell Game" involves cups upturned on a playing surface, with a ball placed underneath one of them. The index of the cups are swapped around multiple times. After that the players will try to find which cup contains the ball.
+
+Your task is as follows. Given the cup that the ball starts under, and list of swaps, return the location of the ball at the end. Cups are given like array/list indices.
+
+For example, given the starting position 0 and the swaps [(0, 1), (1, 2), (1, 0)]:
+
+The first swap moves the ball from 0 to 1
+The second swap moves the ball from 1 to 2
+The final swap doesn't affect the position of the ball.
+So
+
+swaps = [[0,1], [1,2], [1, 0]]
+find_the_ball(0, swaps) == 2
+There aren't necessarily only three cups in this game, but there will be at least two. You can assume all swaps are valid, and involve two distinct indices. */
+
+// SOLUTION //
+
+function findTheBall(start, swaps) {
+    let currentCup = start;
+  
+    for (let swap of swaps) {
+      if (swap[0] === currentCup) {
+        currentCup = swap[1]
+      } else if (swap[1] === currentCup) {
+        currentCup = swap[0]
+      }
+    }
+  
+    return currentCup
+  }
+
+// END //
