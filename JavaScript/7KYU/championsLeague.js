433f6100202b94bf5dfd8bb097f007bafd6c6689
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2024-01-01 07:33:59 -0500

    CodeWars 01-01-2024

---
+++ JavaScript/7KYU/championsLeague.js
@@ -0,0 +1,31 @@
+// CodeWars 01-01-2024 //
+
+/* Description: Create a function that takes two arguments:
+
+An array of objects which feature the season, the team and the country of the Champions League winner.
+
+Country (as a string, for example, 'Portugal')
+
+You function should then return the number which represents the number of times a team from a given country has won. Return 0 if there have been no wins.
+
+For example if the input array is as follows:
+
+
+countWins(winnerList1, 'Spain') => should return 2
+countWins(winnerList1, 'Portugal') => should return 1
+countWins(winnerList1, 'Sportland') => should return 0 */
+
+// SOLUTION //
+
+function countWins(winnerList, country) {
+    let count = 0
+    
+    for(let i = 0; i < winnerList.length; i++) {
+      if(winnerList[i].country == country) {
+        count++
+      }
+    }
+    return count
+  }
+
+// END //
