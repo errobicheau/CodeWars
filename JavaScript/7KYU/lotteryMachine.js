e71ad48255e327a4bc3845413e7616b48c19f3f3
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2024-01-07 13:41:39 -0500

    CodeWars 2024-01-07

---
+++ JavaScript/7KYU/lotteryMachine.js
@@ -0,0 +1,27 @@
+// CodeWars 2024-01-07 //
+
+/* Description: Your task is to write an update for a lottery machine. Its current version produces a sequence of random letters and integers (passed as a string to the function). Your code must filter out all letters and return unique integers as a string, in their order of first appearance. If there are no integers in the string return "One more run!"
+
+Examples
+"hPrBKWDH8yc6Lt5NQZWQ"  -->  "865"
+"ynMAisVpHEqpqHBqTrwH"  -->  "One more run!"
+"555"                   -->  "5" */
+
+// SOLUTION //
+
+function lottery(str){
+    let numArray = []
+    let lottoArray = [...str]
+    
+    for(let char of lottoArray) {
+      if(!isNaN(char) && !numArray.includes(char)) {
+        numArray.push(char)
+      }
+    }
+    
+    numArray = numArray.join('')
+    
+    return numArray.length > 0 ? numArray : 'One more run!'
+  }
+
+// END //
