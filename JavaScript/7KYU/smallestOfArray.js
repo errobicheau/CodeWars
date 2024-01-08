0395df313f486bc1f2d1273ec4152702100bc33b
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2024-01-08 10:41:02 -0500

    CodeWars 2024-01-08

---
+++ JavaScript/7KYU/smallestOfArray.js
@@ -0,0 +1,20 @@
+// CodeWars 2024-01-08 //
+
+/* Description: Write a function that can return the smallest value of an array or the index of that value. The function's 2nd parameter will tell whether it should return the value or the index.
+
+Assume the first parameter will always be an array filled with at least 1 number and no duplicates. Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
+
+min([1,2,3,4,5], 'value') // => 1
+min([1,2,3,4,5], 'index') // => 0 */
+
+
+// SOLUTION //
+
+function min(arr, toReturn) {
+    let arrCopy = [...arr]
+    let sorted = (arrCopy.sort((a, b) => a - b))
+    
+    return toReturn === 'value' ? sorted[0] : arr.indexOf(sorted[0])
+  }
+
+// END //
