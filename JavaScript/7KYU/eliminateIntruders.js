130a845a1bab1b1ff898e87c09958f3b2bc0a165
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2024-01-23 10:03:18 -0500

    CodeWars 2024-01-23

---
+++ JavaScript/7KYU/eliminateIntruders.js
@@ -0,0 +1,32 @@
+// CodeWars 2024-01-23 //
+
+/* Description: You are given a string representing a number in binary. Your task is to delete all the unset bits in this string and return the corresponding number (after keeping only the '1's).
+
+In practice, you should implement this function:
+
+function eliminateUnsetBits(number);
+Examples
+eliminateUnsetBits("11010101010101") ->  255 (= 11111111)
+eliminateUnsetBits("111") ->  7
+eliminateUnsetBits("1000000") -> 1
+eliminateUnsetBits("000") -> 0 */
+
+// SOLUTION //
+
+function eliminateUnsetBits(number) {
+  
+    if(!number.includes(1)) {
+      return 0
+    } else {
+      
+      let ones = number.split('').filter(num => {
+        return num == 1
+        })
+    
+    ones = ones.join('')
+    return parseInt(ones, 2)
+      
+    }
+  }
+
+// END //
